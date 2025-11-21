44b2fe2e321852154dadcbd44b71069ac2e769b8
HenriqueChepli <henriquechepli@gmail.com>
2025-11-21 10:52:01 -0300

    feat: criado inicio do dashboard #1

---
+++ src/views/VueAdmin/HomeView.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { onMounted, computed } from 'vue'
+
+import {
+  NavLateralAdmin,
+  TitleAdmin,
+} from '@/components/index'
+</script>
+
+<template>
+  <div class="flex">
+    <NavLateralAdmin />
+
+    <main class="flex-1 p-6 space-y-6 overflow-hidden">
+      <TitleAdmin
+        title="Painel Administrativo"
+        subtitle="Veja um resumo do que há cadastrado no Sistema"
+      />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+.hide-scrollbar {
+  scrollbar-width: none;
+}
+.hide-scrollbar::-webkit-scrollbar {
+  display: none;
+}
+</style>
